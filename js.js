7b29582366d29a8b1ce14f54fc505d10e7263fe0
Rush2000aitu <57906574+Rush2000aitu@users.noreply.github.com>
2020-02-07 05:33:14 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,31 @@
+/*var url = document.getElementById('id_Элемента');
+document.location.href = url.value;*/
+
+/*function f4(a) {
+
+switch(a) {
+  case 'Faramir':  
+window.open("https://lotr.fandom.com/wiki/Faramir");
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}*/
+/*function f4(){
+var x = document.getElementById("select").value;
+if (a == "Dollar" ){
+document.getElementById('r3').value = (x/380).toFixed(2) ;
+} else if (a == "Euro"){
+document.getElementById('r3').value = (x/420).toFixed(2) ;
+}else if (a == "Sterling"){
+document.getElementById('r3').value = (x/500).toFixed(2) ;
+}
+}*/
 alert("Wolcome!");
 
  function f () {
@@ -10,13 +38,11 @@ return a;
 
  function f1 () {
   let a = document.getElementById('wid').style.display='block';
+
   return a;
  }
- function f3 () {
 
- // if (f4() = true){
- // document.getElementById('pre').style.display='block';
- // }
+ function f3 () {
 
   const list = ["Faramir", "Arwen", "Pippin", "Boromir", "Theoden"
   , "Elrond", "Galadriel", "Merry", "Gimli", "Eowyn", "Legolas"
@@ -29,6 +55,14 @@ for(var i = 0; i < list.length; i++) {
 text += "</ol>";
 document.getElementById("pre").innerHTML = text;
 }
-// function f4 () {
-// document.getElementById('pre').style.display='none';
-// }
+
+function f5(){ 
+ if (document.getElementById("option2").checked == true && 
+ 	document.getElementById("option3").checked == false){
+    alert("Right! Thank You!)))");
+	document.getElementById('form2').style.display='none';
+  } else {
+    alert("You wrong!((( Try again!");
+  }
+}
+
